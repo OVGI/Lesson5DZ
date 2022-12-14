b77d1e86684868455b74686c88647167b5a2cb9c
OVGI <girichevao@gmail.com>
2022-12-14 11:02:36 +0300

    add task34

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+int[] array = new int[4];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+
+int k = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 == 0)
+        k++;
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]}, ");
+Console.Write("]");
+Console.Write($" -> {k}");
+
+
+
